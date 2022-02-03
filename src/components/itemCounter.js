06c2b1060dd53467a3b0e3843fd96f543e48a784
Hammas-Saleem <hammassaleem376@gmail.com>
2022-02-03 19:59:30 +0500

    Update script

--- src/components/itemCounter.js
+++ src/components/itemCounter.js
@@ -1,5 +1,5 @@
 const itemCounter = (totalItems, itemsCounter) => {
-  itemsCounter.innerHTML = `Movies(${totalItems-230})`;
+  itemsCounter.innerHTML = `Movies(${totalItems - 230})`;
 };
 
 export default itemCounter;
